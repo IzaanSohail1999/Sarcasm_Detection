2765d1d5051722549403ec6bfc46bb44042992a5
IzaanSohail10 <izaansohail10.is@gmail.com>
2021-06-06 01:45:43 +0500

    File pushed

--- main.py
+++ main.py
@@ -12,20 +12,40 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import os
-from function import Read_Data_From_File,make_idf,tf_idf_lexicon
+from function import Read_Data_From_File,Read_Data_From_File1,make_idf, tf_idf_lexicon, applyPCA, sort
 
 def main():
    lexicon = Read_Data_From_File()
    # print(lexicon)
-   print(len(lexicon))
    # idf = make_idf(lexicon)
-   # print(len(idf))
-   # print(idf)
+   # print("Idf made")
    # tf_idf = tf_idf_lexicon(lexicon,idf)
-   # print(tf_idf)
-   # print(len(tf_idf))
-   # print(tf_idf.keys())
-    
-        
+   # print("tf_Idf made")
+   # # tf_idf = sort(tf_idf)
+   # with open("TFIDF_Dictionary.json", "w") as f: # Writing the index to JSON File.
+   #    f.write(json.dumps(tf_idf, sort_keys=False, indent=4)) 
+   # with open('TFIDF_Dictionary.json') as json_file:
+   #    Dict = json.load(json_file)
+   # Dict = sort(Dict)
+   # print(Dict)
+   # with open("TFIDF_Dictionary_new.json", "w") as f:
+      # f.write(json.dumps(Dict, sort_keys=False, indent=4))
+   
+   #Create Panda DataFrame
+   # df = pd.DataFrame(tf_idf)
+   # df = np.transpose(df)
+   # print(df)
+
+   # print("Data Processing Completed........")
+   # print("")
+   # print("Applying PCA.....................")
+
+   # x_pca, scaled_data = applyPCA(df)
+   
+   # print(x_pca,scaled_data)
+   # print("")
+   # print("PCA Applied......................")
+   # print("")
+   # print("Traning Model....................")
 if __name__ == "__main__":
     main()
